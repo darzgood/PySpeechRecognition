d2f3b59a11e097d26ea6fa320c8abee0e40c5d3e
Darrell Good <darzgood@gmail.com>
2019-04-12 10:43:11 -0400

    finalized mp3 saving

--- listFiles.py
+++ listFiles.py
@@ -1,10 +1,28 @@
 import os
 
+from gtts import gTTS
+import os
+
+tts = gTTS(text="Would you like a spot of tea?", lang='en')
+tts.save("pcvoice.mp3")
+
 def mp3gen():
     for root, dirs, files in os.walk('.'):
         for filename in files:
             if os.path.splitext(filename)[1] == ".mp3":
                 yield os.path.join(root, filename)
 
-for mp3file in mp3gen():
-    print(mp3file)
+def saveName(name, filename):
+    tts = gTTS(text=name, lang='en')
+    name = "{0}(Name) - {1}.mp3".format(filename, name)
+    tts.save(name)
+    print(name)
+
+def main():                
+    for mp3file in mp3gen():
+        parts = mp3file[2:].split(" - ")
+        if len(parts) == 3:
+            saveName(parts[1], parts[0])
+            
+if __name__ == "__main__":
+    main()
